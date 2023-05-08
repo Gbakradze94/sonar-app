080fdd76ce6a75194f9da153bfba9e1d9cbef319
Giorgi Bakradze <giorgi_bakradze@epam.com>
2023-05-08 12:41:46 +0400

    Initial Commit

---
+++ src/main/java/com/epam/training/sonarapp/SonarAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.training.sonarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/training/sonarapp/SonarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.training.sonarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
